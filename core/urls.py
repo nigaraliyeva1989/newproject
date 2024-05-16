cf7423516ce41c9786f400069bb9126bf8f25e7c
nigaraliyeva1989 <nigarfarizsm@gmail.com>
2024-05-16 23:29:34 +0400

    feat:create contact application

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import index, contact
+from .views import index
 
 urlpatterns = [
     path('', index,name="index"),
-    path('contact', contact,name="contact")
+
 ]
 
 
